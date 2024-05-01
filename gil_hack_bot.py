afc25a3684b1d3ec67ef6a5fbd8a5f7c9c0bc8bf
gilbert866 <amo_gilbert@yahoo.com>
2024-05-01 13:17:38 +0000

    Trying a DevOPs project by creationg a bot

---
+++ gil_hack_bot.py
@@ -0,0 +1,46 @@
+import os
+import telebot
+import requests
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+def get_daily_horoscope(sign: str, day: str) -> dict:
+    """Get daily horoscope for a specific sign.
+    Keyword arguments:
+    sign:str - Zodiac Sign
+    day:str - Date in formatt(yyyy-mm-dd) or TODAY or TOMORROW or YESTERDAY
+    Returns: dict - JSON data
+    """
+    url = "https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily"
+    params = {"sign": sign, "day": day}
+    response = requests.get(url, params)
+
+    return response.json()
+
+
+@bot.message_handler(commands=["horoscope"])
+def sign_handler(message):
+    text = "What's your zodiac sign?\nChoose one: *Aries*, *Taurus*, *Gemini*, *Cancer,* *Leo*, *Virgo*, *Libra*, *Scorpio*, *Sagittarius*, *Capricorn*, *Aquarius*, and *Pisces*."
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, day_handler)
+
+
+def day_handler(message):
+    sign = message.text
+    text = "What day do you want to know?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date in format YYYY-MM-DD."
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, fetch_horoscope, sign.capitalize())
+
+
+def fetch_horoscope(message, sign):
+    day = message.text
+    horoscope = get_daily_horoscope(sign, day)
+    data = horoscope["data"]
+    horoscope_message = f'*Horoscope:* {data["horoscope_data"]}\\n*Sign:* {sign}\\n*Day:* {data["date"]}'
+    bot.send_message(message.chat.id, "Here's your horoscope!")
+    bot.send_message(message.chat.id, horoscope_message, parse_mode="Markdown")
+
+
+bot.infinity_polling()
